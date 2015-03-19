6e513e18840e90f117d2ae7d498f2ee7fd4da284
PStanos <paulstanos@gmail.com>
2015-03-19 01:01:53 -0400

    Initial commit

---
+++ app/src/main/java/edu/msu/stanospa/cloudhatter/LoadDlg.java
@@ -0,0 +1,55 @@
+package edu.msu.stanospa.cloudhatter;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by Paul on 3/19/2015.
+ */
+public class LoadDlg extends DialogFragment {
+    /**
+     * Create the dialog box
+     * @param savedInstanceState The saved instance bundle
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // Set the title
+        builder.setTitle(R.string.load_fm_title);
+
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Pass null as the parent view because its going in the dialog layout
+        @SuppressLint("InflateParams")
+        View view = inflater.inflate(R.layout.catalog_dlg, null);
+        builder.setView(view);
+
+        // Add a cancel button
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // Cancel just closes the dialog box
+            }
+        });
+
+        AlertDialog dlg = builder.create();
+
+        // Find the list view
+        ListView list = (ListView)view.findViewById(R.id.listHattings);
+
+        // Create an adapter
+        final Cloud.CatalogAdapter adapter = new Cloud.CatalogAdapter(list, this);
+        list.setAdapter(adapter);
+
+        return dlg;
+    }
+}
